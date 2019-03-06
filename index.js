7c79ef5934a987b13863b6f48920e7cc1389f105
Stephen Wool <stevewool7@gmail.com>
2019-03-06 14:10:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function iterativeLog(array) {
   array.forEach(function(element, index) {
-    console.log(`${index}: ${element}`)
-  })
+    console.log(`${index}: ${element}`);
+  });
 }
 
 function iterate(callback) {
   var array = [1, 2, 3];
-  array.forEach(callback)
-  return array
+  array.forEach(callback);
+  return array;
 }
